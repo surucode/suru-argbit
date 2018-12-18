50b2851cee6ff5896fa8a97f188d9ce7caa328b2
Giovanni Olivera <giovanni@commit42.fr>
2018-12-18 14:17:38 +0100

    wip

---
+++ suru.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const process = require("process");
+const path = require("path");
+
+task(() => {
+    name("build");
+    desc("build suru with suru");
+
+    shell("npx", "tsc", "-d");
+
+    run(() => {
+        const package_json = fs.readFileSync(__dirname + "/package.json", {
+            encoding: "utf-8",
+            flag: "r"
+        });
+        const package = JSON.parse(package_json);
+        package.main = "index.js";
+        package.types = "index.d.ts";
+
+        delete package.devDependencies;
+        delete package.scripts;
+
+        fs.writeFileSync(
+            __dirname + "/dist/package.json",
+            JSON.stringify(package, null, 3)
+        );
+
+        try {
+            fs.unlinkSync(__dirname + "/dist/package-lock.json");
+        } catch (err) {
+            console.error(err);
+        }
+    });
+});
+
+task(() => {
+    name("publish");
+    desc("publish suru-argbit");
+
+    shell("npm", "install");
+
+    invoke("build")();
+
+    run(() => {
+        console.log(process.cwd());
+        process.chdir(path.resolve("./dist", __dirname));
+        console.log(process.cwd());
+        shell("npm", "publish", "@surucode/suru-argbit");
+    });
+
+});
+
+task(() => {
+    name("test");
+    desc("test with jest");
+
+    shell("npm", "t", "--", "--watch");
+});
